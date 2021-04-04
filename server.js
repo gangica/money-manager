7025d0fd37d705f5993c214e8206c2147dd32213
Jayden Cao <trex.icarus@gmail.com>
2021-04-05 01:28:30 +0700

    update 8

--- server.js
+++ server.js
@@ -26,6 +26,21 @@ const connectDB = async () => {
 
 connectDB();
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+    app.get('/:id', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 // get transactions api from router
 app.use('/api/v1/transactions', transactions);
 
